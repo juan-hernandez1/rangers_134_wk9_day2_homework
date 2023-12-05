a20b56aac5acd4994c06781fa1f7bef612e2fd32
juan-hernandez1 <juan.c.hernandez.312@gmail.com>
2023-12-05 17:27:34 -0600

    first commit

---
+++ src/components/Garage/Gargage.tsx
@@ -0,0 +1,11 @@
+import * as _React from 'react'
+// import React from 'react' 
+
+
+export const Garage = () => {
+    return (
+        <div>
+            <h1 style={{marginTop: '50px'}}>This is the Garage</h1>
+        </div>
+    )
+}
